6a212552aae977a99a524caed563ee394fc85758
Lyzog <xddaniel999@gmail.com>
2019-03-26 13:38:09 +0100

    first commit

---
+++ app/src/main/java/com/example/guess_the_place/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.guess_the_place;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMap();
+            }
+        });
+    }
+    public void openMap(){
+        Intent intent = new Intent(this,MapsActivity.class);
+                startActivity(intent);
+
+    }
+}
