a9a3875d53b3477de603871e7d7da43b414d4a9c
Lyzog <xddaniel999@gmail.com>
2019-04-04 16:03:14 +0200

    change button to guess (MainActivity.java(16))
    added MenuActivity with almost the same things that are in MainActivity
    
    new picture (drawable(eiffeltower.jpg))
    
    added globe_and_pin.jpg(drawable)
    
    change from ConstraintLayout to RelativeLayout and
    removed TextView hello world and
    change id button to Guess
    change string button to Guess
    (activity_main.xml)
    
    change layout_height from 195dp to 130dp (activity_maps.xml(17))
    
    added a new activity with a button, image and text(activity_menu.xml)
    
    added 3 new strings (string.xml(5-7))
    
    added and made MenuActivity the first Activity to open in the app instead of MainActivity

--- app/src/main/java/com/example/guess_the_place/MenuActivity.java
+++ app/src/main/java/com/example/guess_the_place/MenuActivity.java
@@ -1,30 +1,30 @@
 package com.example.guess_the_place;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
 
-        Button button = findViewById(R.id.Guess);
+        Button button = findViewById(R.id.start);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openMap();
+                openMain();
             }
         });
     }
 
-    public void openMap(){
-        Intent intent = new Intent(this,MapsActivity.class);
-                startActivity(intent);
+    public void openMain(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
 
     }
 }
