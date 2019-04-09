6d836df52a89780fb4ac35d100cc3519f81764ce
Lyzog <xddaniel999@gmail.com>
2019-04-09 11:12:06 +0200

    ===================  MapsActivity.java ===================
    imported DecimalFormat (18-21)
    
    added Context (25)
    
    fixed the distance so its rounding to 1-2 decimals (72-76)(78-81)
    
    added an alert that displays the distance between the pins (83-95)(99-111)
    
    ===================
     activity_main.xml
    ===================
    
    fixed the position (12)(14-16)(18)(20)
    
    added an ImageView (27-35)
    
    =============
     strings.xml
    =============
    
    added some strings (8-18)

--- app/src/main/java/com/example/guess_the_place/MapsActivity.java
+++ app/src/main/java/com/example/guess_the_place/MapsActivity.java
@@ -1,7 +1,11 @@
 package com.example.guess_the_place;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -12,8 +16,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.text.DecimalFormat;
+
+import static java.lang.Math.round;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    final Context context = this;
     private GoogleMap mMap;
 
     @Override
@@ -60,14 +69,49 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 double x = (eiffelTower.longitude - point.longitude) * Math.cos((eiffelTower.latitude + point.latitude) / 2);
                 double y = (eiffelTower.latitude - point.latitude);
-                double distance = Math.sqrt(x * x + y * y)* 100000;
+                double distance = Math.round(Math.sqrt(x * x + y * y)* 100000);
+                double distance1 = (Math.sqrt(x * x + y * y)* 100000);
 
-                if (distance >= 1000){
+                DecimalFormat df = new DecimalFormat("0.0");
+                String d0 = df.format(distance1);
+
+                DecimalFormat df1 = new DecimalFormat("0.00");
+                String d1 = df1.format(distance1);
+                
+                if (distance <= 1000){
                     Log.d("!!!", "distance change to km: " + distance / 1000);
+
+                    AlertDialog.Builder alert = new AlertDialog.Builder(context);
+                    alert.setMessage("you points: " + d0 + "m");
+                    alert.setCancelable(true);
+                    alert.setPositiveButton("end", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent(MapsActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        }
+                    });
+                    AlertDialog alert1 = alert.create();
+                    alert1.show();
 
                 } else {
                     Log.d("!!!", "distance: " + distance);
+
+                    AlertDialog.Builder alert = new AlertDialog.Builder(context);
+                    alert.setMessage("distance: " + d1 + "km");
+                    alert.setCancelable(true);
+                    alert.setPositiveButton("end", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent(MapsActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        }
+                    });
+                    AlertDialog alert1 = alert.create();
+                    alert1.show();
                 }
+
+
             }
 
         });
